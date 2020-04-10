38ff62c621e660b250e27bb54282e9019e7bb839
Evgenii P <eupn@protonmail.com>
2020-04-10 21:49:21 +0800

    Stabilize to 1.0.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![crate_type = "lib"]
-#![doc(html_root_url = "https://docs.rs/macrotest/0.1.6")]
+#![doc(html_root_url = "https://docs.rs/macrotest/1.0.0")]
 
 //! #### &emsp; Test harness for macro expansion.
 //!
