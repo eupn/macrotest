e53bcf09e2f8a9d53494673aaf3f517cb7c420df
Evgenii P <eupn@protonmail.com>
2020-10-30 00:08:10 +0700

    Bump version 1.0.5 -> 1.0.6

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![crate_type = "lib"]
-#![doc(html_root_url = "https://docs.rs/macrotest/1.0.5")]
+#![doc(html_root_url = "https://docs.rs/macrotest/1.0.6")]
 
 //! #### &emsp; Test harness for macro expansion.
 //!
