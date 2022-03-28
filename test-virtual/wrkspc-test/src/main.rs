f67ac0f150765d46476eb7fd5ee4cf11264513af
Mark Van de Vyver <mark@taqtiqa.com>
2022-03-28 17:05:22 +1100

    Add: Initial Cargo test harness
    Running:
    
    cargo test workspace-tests
    
    correctly produces:
    
         Running src/main.rs (target/debug/deps/workspace_tests-8bdd65c7ce129a54)
    Setup
    Teardown
    
    Signed-off-by: Mark Van de Vyver <mark@taqtiqa.com>

---
+++ test-virtual/wrkspc-test/src/main.rs
@@ -0,0 +1,16 @@
+fn setup() {
+   println!("Setup")
+}
+
+fn teardown() {
+   println!("Teardown")
+}
+fn main() {
+   // Setup test environment
+   setup();
+
+   // TODO: Run the test
+
+   // Teardown test environment
+   teardown();
+}
